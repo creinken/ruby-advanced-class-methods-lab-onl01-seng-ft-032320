916fe25fc094013ecd0ff4f95d5323b7db66d13a
Christopher Reinken <christopherreinken@gmail.com>
2020-04-01 13:01:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
   end
   
   def self.find_by_name(song_title)
-    Song.all.any?(song_title)
+    Song.all.name.any?(song_title)
   end
   
   def self.find_or_create_by_name(song_title)
