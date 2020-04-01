cafc66d6705d4afb74e6e3a71ff7a850a304cbdf
Christopher Reinken <christopherreinken@gmail.com>
2020-04-01 12:37:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,43 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.create
+    @@all << self
+    self
+  end
+  
+  def self.new_by_name(song)
+    
+  end
+  
+  def self.create_by_name(song)
+    
+  end
+  
+  def self.find_by_name(song)
+    
+  end
+  
+  def self.find_or_create_by_name(song)
+    
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|a,b| a <=> b}
+  end
+  
+  def self.new_from_filename(file_name)
+    
+  end
+  
+  def self.create_from_filename(file_name)
+  
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 
   def save
     self.class.all << self
